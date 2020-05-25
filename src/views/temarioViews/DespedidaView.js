32cbc506b61919669f12a5b9dcc35ff2e72f5efe
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-25 14:54:52 -0500

    Update DespedidaView.js

--- src/views/temarioViews/DespedidaView.js
+++ src/views/temarioViews/DespedidaView.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { CardBody, CardHeader, Col, Row } from 'reactstrap'
+import ReactPlayer from 'react-player'
+import cierre01 from '../../assets/img/ImgClientesDificiles/cierre01.mp4'
 
 class PresentacionView extends Component {
   loading = () => (
@@ -24,21 +26,17 @@ class PresentacionView extends Component {
             </Col>
             <Col xs="12" className="centrado-fila">
               <div
-                style={{ width: '400px' }}
+                style={{ maxWidth: '800px' }}
                 className="cajaA2 bg-fondo-btn2 animated  fadeInUp  delay-2s"
               >
                 <p className="text-justify animated  fadeInDown  delay-3s ">
-                  Esta variabilidad es la que obligaría a las empresas a crear
-                  productos y servicios con tantas modificaciones como personas
-                  hay, algo objetivamente imposible. Ello, nos lleva a la
-                  necesidad de conjugar dicha diferenciación con las
-                  posibilidades reales de atención al cliente y para ello, una
-                  de las soluciones encontrada es la de agrupar a los clientes,
-                  según una serie de criterios comunes. Se trata de establecer
-                  distintas tipologías de clientes con unas motivaciones y
-                  actitudes similares, aunque sea a grandes rasgos.
+                Esta variabilidad es la que obligaría a las empresas a crear productos y servicios con tantas modificaciones como personas hay, algo objetivamente imposible. Ello, nos lleva a la necesidad de conjugar dicha diferenciación con las posibilidades reales de atención al cliente y para ello, una de las soluciones encontrada es la de agrupar a los clientes, según una serie de criterios comunes.
+Se trata de establecer distintas tipologías de clientes con unas motivaciones y actitudes similares, aunque sea a grandes rasgos.
                 </p>
               </div>
+            </Col>
+            <Col xs="12" className="centrado-fila mt-4 animated  fadeInDown  delay-5s">
+              <ReactPlayer url={cierre01} controls={true} />
             </Col>
           </Row>
         </CardBody>
